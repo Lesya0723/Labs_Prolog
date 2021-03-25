21d89a287de5fb5ae156d3bc361bc5f0b3e46d60
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-03-25 21:40:45 +0300

    Задание 1

---
+++ Lab7/lab.pl
@@ -0,0 +1,9 @@
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+wr_str3:-read_str(A,Length),write_str(A),write(","),write_str(A),write(","),write_str(A),write(","),write(Length).
+
