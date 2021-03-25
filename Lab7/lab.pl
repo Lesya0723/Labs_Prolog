a7138eea36c138962f4b4396cbf1b675ed805e40
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-03-25 21:54:59 +0300

    Задание 2

--- Lab7/lab.pl
+++ Lab7/lab.pl
@@ -5,5 +5,12 @@ read_str(A,N):-get0(X),r_str(X,A,[],N,0).
 r_str(10,A,A,N,N):-!.
 r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 
+append1([],List,List):-!.
+append1([H1|Tail],List,[H1|Tail1]):-append(Tail,List,Tail1).
+
 wr_str3:-read_str(A,Length),write_str(A),write(","),write_str(A),write(","),write_str(A),write(","),write(Length).
+
+kolvo_words:-read_str(A,_),append1([32],A,A1),kol_slov(A1,0,Kol),write(Kol).
+kol_slov([_|[]],Kol,Kol):-!.
+kol_slov([H1|[H2|T]],K,Kol):-(H1=32,H2\=32 -> K1 is K+1,kol_slov([H2|T],K1,Kol);kol_slov([H2|T],K,Kol)).
 
