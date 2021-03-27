e6718d1852dbaa0acdf6fa0856b8536e9b366b9b
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-03-27 20:05:39 +0300

    Задание 2.2

--- Lab8/lab8.pl
+++ Lab8/lab8.pl
@@ -1,3 +1,13 @@
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
 ex1:-see('C:/Users/Admin/Documents/Prolog/лр8/in.txt'),read_list_str(List),seen,ex1(List,0,Res),write(Res),told.
 ex1([],Res,Res):-!.
 ex1([H|T],K,Res):-length(H,L),(L>K -> K1 is L,ex1(T,K1,Res);ex1(T,K,Res)).
@@ -10,4 +20,8 @@ count_space([]):-!.
 count_space([H|_]):-H is 32,!,fail.
 count_space([_|T]):-count_space(T).
 
-
+ex2_2:-see('C:/Users/Admin/Documents/Prolog/лр8/in.txt'),read_str(S,_,1),seen,
+ex2_2(S),told.
+ex2_2([_]):-write("Символы упорядочены"),!.
+ex2_2([H1,H2|T]):-H1<H2,!,ex2_2([H2|T]).
+ex2_2([_,_|_]):-write("Символы не упорядочены"),!.
