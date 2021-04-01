5fb15cc7858423ab61746448966d9471f30a5a31
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2021-04-01 22:37:43 +0300

    Задание 8

--- Lab9/lab9.pl
+++ Lab9/lab9.pl
@@ -109,3 +109,7 @@ put_pos93(Word,[H1,H2,H3],[Res]):-select_pos9(Word,H1,Res),select_pos9(Word,H2,R
 in_free_pos9([H1,H2,H3,H4,H5,H6,H7,H8,H9],Sim):-(var(H1)->H1 is Sim),!;(var(H2)->H2 is Sim),!;(var(H3)->H3 is Sim),!;(var(H4)->H4 is Sim),!;(var(H5)->H5 is Sim),!;(var(H6)->H6 is Sim),!;
 (var(H7)->H7 is Sim),!;(var(H8)->H8 is Sim),!;(var(H9)->H9 is Sim).
 
+ex8:-tell('C:/Users/Admin/Documents/Prolog/лр9/in.txt'),write_str([[97],[97],[97],[97]]),nl,not(exr8),told.
+exr8:- make(4,Pos),in_list(Pos,I),in_list([98,99,100,101,102],Simb1),put_pos4(Word,[I],[Simb1]), in_list_exlude([98,99,100,101,102],Simb1,_),		 in_free_pos4(Word,[97]),		 in_free_pos4(Word,[97]),		 in_free_pos4(Word,[97]),		 write_str(Word),nl,fail.
+put_pos4(Word,[H1],[Res]):-select_pos4(Word,H1,Res).select_pos4(Word,H,Sim):-(H is 1->Word=[Sim,_,_,_],!);(H is 2->Word=[_,Sim,_,_],!);(H is 3->Word=[_,_,Sim,_],!);(H is 4->Word=[_,_,_,Sim],!).
+in_free_pos4([H1,H2,H3,H4],Sim):-(var(H1)->H1 is Sim),!;(var(H2)->H2 is Sim),!;(var(H3)->H3 is Sim),!;(var(H4)->H4 is S
